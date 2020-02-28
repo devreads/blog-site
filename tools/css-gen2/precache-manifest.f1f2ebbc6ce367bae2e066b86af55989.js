47dd602fe44b28fa06824897eb7465cfff8d4070
DevReads <52862578+devreads@users.noreply.github.com>
2020-02-28 13:14:25 +0530

    Add files via upload

---
+++ tools/css-gen2/precache-manifest.f1f2ebbc6ce367bae2e066b86af55989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c2802521960a88655fcfba254ca69c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2dd4fb5249fea48209a",
+    "url": "/static/css/main.7e78e791.chunk.css"
+  },
+  {
+    "revision": "38ba8247956c49c05162",
+    "url": "/static/js/2.17e28c0d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.17e28c0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2dd4fb5249fea48209a",
+    "url": "/static/js/main.28f4592a.chunk.js"
+  },
+  {
+    "revision": "804714100f88ff916167",
+    "url": "/static/js/runtime-main.e95a25fd.js"
+  }
+]);
