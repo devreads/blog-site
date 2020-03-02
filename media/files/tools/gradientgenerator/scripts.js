f6a4a09228e3981ec34c2746f0f81113345480dd
DevReads <52862578+devreads@users.noreply.github.com>
2020-03-02 16:36:23 +0530

    Updated from Publii

--- media/files/tools/gradientgenerator/scripts.js
+++ media/files/tools/gradientgenerator/scripts.js
@@ -22,31 +22,41 @@ let code
 let percentageA = document.getElementById("range-a"),
 percentageB = document.getElementById("range-b");
 
+function updateCssCode(){
+	let code = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+	document.getElementById("css-code").innerText = code;
+	document.getElementById("clipboard-alert").innerText = "Copy to clipboard";
+}
+
 for(let i = 0; i < direction.length; i++ ) {
 
     direction[i].addEventListener("click", (event) => {
         document.getElementById("direction-input").innerText = `${direction[i].value}`;
         document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
-    });
-
+		updateCssCode();
+	});
 };
 
 pickerA.addEventListener("input", (event) => {
     hexTextA.innerHTML = pickerA.value;
     document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+	updateCssCode();
 }, false);
 
 pickerB.addEventListener("input", (event) => {
     hexTextB.innerHTML = pickerB.value;
     document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+	updateCssCode();
 }, false);
 
 percentageA.addEventListener("input", (event) => {
     document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+	updateCssCode();
 });
 
 percentageB.addEventListener("input", (event) => {
     document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+	updateCssCode();
 });
 
 generate.addEventListener("click", (event) => {
@@ -56,6 +66,6 @@ generate.addEventListener("click", (event) => {
     document.body.appendChild(codeToCopy);
     codeToCopy.select();
     document.execCommand('copy');
-    alert(`The following code has been copied to your clipboard: "${code}"`);
+	document.getElementById("clipboard-alert").innerText = "Copied..";
     document.body.removeChild(codeToCopy);
 })
